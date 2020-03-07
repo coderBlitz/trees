6149cc92df0fcf8d0058928d7e1817efb93ce453
Christopher Skane <sokrbball@gmail.com>
2020-03-06 19:00:33 -0500

    Added remaining copyright text.

--- avl.c
+++ avl.c
@@ -1,3 +1,21 @@
+/*
+	avl.c -- AVL tree that stores a single piece of data
+
+	Copyright (C) 2019 Christopher Skane
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	Full license at https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+*/
+
 #include"avl.h"
 
 size_t updateHeight(struct node *root){
